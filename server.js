8e83bd418673e7ebac40101bbee8d779a70d0bb1
Simon <simon@bottbottgenai.com>
2026-02-16 14:25:21 -0600

    Use dynamic port for Railway

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.post('/analyze', (req, res) => {
   });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Express server running on http://localhost:${PORT}`);
 });
